d5ce6c483100e6c4f05317b87da4ef55c57c4ac8
boutchekkoucht <boutchekkoucht@gmail.com>
2015-12-17 09:59:48 +0000

    commentaire listener

--- src/com/ensas/ecommerce/listeners/ButtonListener.java
+++ src/com/ensas/ecommerce/listeners/ButtonListener.java
@@ -3,6 +3,9 @@ package com.ensas.ecommerce.listeners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+
+// ici tous les listeners buttons
 public class ButtonListener implements ActionListener{
 
 	public ButtonListener() {
